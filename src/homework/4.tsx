a3cbcb7c23658a8caa7afc03e29ce7af7bc8e3a8
Olena <shapovalev@gmail.com>
2023-11-02 20:15:49 +0100

    hw-04

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -5,6 +5,8 @@ type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
+type SelectedMenu = { id?: MenuIds };
+type MenuSelected = { selectedMenu: SelectedMenu };
 
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
@@ -12,12 +14,16 @@ const MenuSelectedContext = createContext<MenuSelected>({
 
 // Додайте тип MenuAction
 
+type MenuAction = {
+  onSelectedMenu: (selectedMenu: SelectedMenu) => void;
+};
+
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +54,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
